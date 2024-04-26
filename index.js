41105b9d76bc2caf027557a22c313304dd72ad97
Chessin8tor <63427758+Chessin8tor@users.noreply.github.com>
2024-04-26 08:17:16 -0700

    Update locations

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ class LockerLocator {
     this.scale = 1;
     this.lastTouchDistance = 0;
     this.categoryPositions = {
-      Restrooms: [{ top: 185, left: 290, width: 8, height: 21 }, { top: 191, left: 395, width: 10, height: 15 }, 
+      'Restrooms': [{ top: 185, left: 290, width: 8, height: 21 }, { top: 191, left: 395, width: 10, height: 15 }, 
         { top: 309, left: 96, width: 18, height: 10 }, { top: 345, left: 130, width: 18, height: 10 },
         { top: 404, left: 164, width: 29, height: 9 }, { top: 389, left: 530, width: 17, height: 10 },
         { top: 517, left: 508, width: 22, height: 8 },], 
-        'Attendance Office': [{ top: 440, left: 165, width: 25, height: 23 }],
-        'Parking Lots': [{ top: 140, left: 90, width: 100, height: 80 }],
+      'Attendance Office': [{ top: 440, left: 165, width: 25, height: 23 }],
+      'Parking Lots': [{ top: 140, left: 90, width: 100, height: 80 }, { top: 485, left: 60, width: 175, height: 55 },],
         // Add other categories as needed
       };
       
